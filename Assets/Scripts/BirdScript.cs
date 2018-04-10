3788783da35e5d84fa8723c0429259fd51edd7a2
Rjmorcast <32773693+Rjmorcast@users.noreply.github.com>
2018-04-10 08:20:33 -0600

    Juego fase1

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    public float jumpForce = 400f;
+    public float forwardSpeed = 2f;
+    Rigidbody2D rb;
+    public GameObject cam;
+    bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (rb.transform.localPosition.x >= 20) {
+            dead = true;
+        }
+        if (!dead) { 
+            //rb.velocity = Vector2.zero;
+            rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+            cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(0, 1) * jumpForce);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        dead = true;
+    }
+
+}
